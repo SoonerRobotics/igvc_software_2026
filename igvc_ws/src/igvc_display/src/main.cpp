83954d7e2f42f7fce3a22b165d8db7bc08456e75
Dylan Zemlin <dylan.zemlin@ou.edu>
2025-10-11 16:37:39 -0500

    rearranged some ui layout stuff

--- igvc_ws/src/igvc_display/src/main.cpp
+++ igvc_ws/src/igvc_display/src/main.cpp
@@ -8,10 +8,11 @@
 #include "igvc_display/clayrib.h"
 
 // helpful constants and helpers
-#define RAYLIB_VECTOR2_TO_CLAY_VECTOR2(vector) (Clay_Vector2) { .x = vector.x, .y = vector.y }
-#define COLOR_ORANGE (Clay_Color) {225, 138, 50, 255}
-#define COLOR_BLUE (Clay_Color) {111, 173, 162, 255}
-Clay_TextElementConfig headerTextConfig = { .textColor = {0,0,0,255}, .fontId = 0, .fontSize = 16, .letterSpacing = 5 };
+#define RAYLIB_VECTOR2_TO_CLAY_VECTOR2(vector) \
+    (Clay_Vector2) { .x = vector.x, .y = vector.y }
+#define COLOR_ORANGE (Clay_Color){225, 138, 50, 255}
+#define COLOR_BLUE (Clay_Color){111, 173, 162, 255}
+Clay_TextElementConfig headerTextConfig = {.textColor = {0, 0, 0, 255}, .fontId = 0, .fontSize = 24};
 
 using namespace std::chrono_literals;
 
@@ -37,45 +38,58 @@ private:
     {
         Clay_BeginLayout();
 
-        // create a horizonal header bar
-        CLAY_AUTO_ID({ .layout = { .sizing = { .width = CLAY_SIZING_GROW(1), .height = CLAY_SIZING_GROW(0) }, .padding = {8, 8, 8, 8 }, .childGap = 8, .childAlignment = { .x = CLAY_ALIGN_X_RIGHT } }, .backgroundColor =  {180, 180, 180, 255} }) {            
-            RenderHeaderButton(CLAY_STRING("Header Item 1"));
-            RenderHeaderButton(CLAY_STRING("Header Item 2"));
-            RenderHeaderButton(CLAY_STRING("Header Item 3"));
-            RenderHeaderButton(CLAY_STRING("Header Item 4"));
-            RenderHeaderButton(CLAY_STRING("Header Item 5"));
+        CLAY_AUTO_ID({.layout = {.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_GROW()}, .layoutDirection = CLAY_TOP_TO_BOTTOM}})
+        {
+            // create a horizonal header bar
+            CLAY_AUTO_ID({.layout = {.sizing = {.width = CLAY_SIZING_GROW()}, .padding = {8, 8, 8, 8}, .childGap = 8, .childAlignment = {.x = CLAY_ALIGN_X_RIGHT}}, .backgroundColor = {180, 180, 180, 255}})
+            {
+                RenderHeaderButton(CLAY_STRING("Header Item 1"));
+                RenderHeaderButton(CLAY_STRING("Header Item 2"));
+                RenderHeaderButton(CLAY_STRING("Header Item 3"));
+                RenderHeaderButton(CLAY_STRING("Header Item 4"));
+                RenderHeaderButton(CLAY_STRING("Header Item 5"));
+            }
+
+            // main content area
+            CLAY_AUTO_ID({.layout = {.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_GROW(1)}, .padding = {16, 16, 16, 16}}, .backgroundColor = {240, 240, 240, 255}})
+            {
+                CLAY_TEXT(CLAY_STRING("Main Content Area"), CLAY_TEXT_CONFIG(headerTextConfig));
+            }
         }
 
         return Clay_EndLayout();
     }
 
-    Clay_ElementDeclaration HeaderButtonStyle(bool hovered) {
-        return (Clay_ElementDeclaration) {
+    Clay_ElementDeclaration HeaderButtonStyle(bool hovered)
+    {
+        return (Clay_ElementDeclaration){
             .layout = {.sizing = {.width = CLAY_SIZING_GROW(1), .height = CLAY_SIZING_GROW(0)}, .padding = {16, 16, 8, 8}},
             .backgroundColor = hovered ? COLOR_ORANGE : COLOR_BLUE,
         };
     }
 
     // Examples of re-usable "Components"
-    void RenderHeaderButton(Clay_String text) {
-        CLAY_AUTO_ID(HeaderButtonStyle(Clay_Hovered())) {
+    void RenderHeaderButton(Clay_String text)
+    {
+        CLAY_AUTO_ID(HeaderButtonStyle(Clay_Hovered()))
+        {
             CLAY_TEXT(text, CLAY_TEXT_CONFIG(headerTextConfig));
         }
     }
 
     void init()
     {
-        Clay_Raylib_Initialize(1024, 768, "IGVC 2026", 0);
+        Clay_Raylib_Initialize(1920, 1080, "IGVC 2026", FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 
         // create clay arena and context
         uint64_t required_mem = Clay_MinMemorySize();
         Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(required_mem, malloc(required_mem));
-        Clay_Context *clayContext = Clay_Initialize(arena, (Clay_Dimensions){.width = 1024, .height = 768}, (Clay_ErrorHandler){HandleClayErrors});
+        Clay_Context *clayContext = Clay_Initialize(arena, (Clay_Dimensions){.width = 1920, .height = 1080}, (Clay_ErrorHandler){HandleClayErrors});
 
         // load and create fonts
         Font fonts[1];
-        fonts[0] = LoadFontEx("build/igvc_display/resources/Roboto.ttf", 48, 0, 400);
-        SetTextureFilter(fonts[0].texture, TEXTURE_FILTER_BILINEAR);
+        fonts[0] = LoadFontEx("build/igvc_display/resources/Roboto.ttf", 24, 0, 250);
+        SetTextureFilter(fonts[0].texture, TEXTURE_FILTER_ANISOTROPIC_4X);
         Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
 
         // set image icon
@@ -109,7 +123,7 @@ private:
             // Mouse Stuff
             Clay_Vector2 mousePosition = RAYLIB_VECTOR2_TO_CLAY_VECTOR2(GetMousePosition());
             Clay_SetPointerState(mousePosition, IsMouseButtonDown(0));
-            Clay_SetLayoutDimensions((Clay_Dimensions) { (float)GetScreenWidth(), (float)GetScreenHeight() });
+            Clay_SetLayoutDimensions((Clay_Dimensions){(float)GetScreenWidth(), (float)GetScreenHeight()});
 
             Clay_Raylib_Render(renderCommands, fonts);
 
