66cb4841f81a5c0fd47418a0dc7334e2e2350ccd
Dylan Zemlin <dylan.zemlin@gmail.com>
2026-01-09 15:06:49 -0600

    new scripts

---
+++ setup/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# General
+sudo apt update
+
+# Dependencies
+sudo apt install git -y
+sudo apt install unzip -y
+sudo apt install curl -y
+sudo apt install cmake -y
+sudo apt install build-essential -y
