c958c7a1fd8b82675f1eba64f260676dc9326c82
Daniel Brown <is.a.squrril@gmail.com>
2026-04-26 17:03:04 -0500

    add feelers to configuration system

--- igvc_csharp/src/Configuration.cs
+++ igvc_csharp/src/Configuration.cs
@@ -26,7 +26,7 @@ public static class Configuration
     /// A magic header for all networking nonsense
     /// </summary>
     public static readonly byte[] NetworkingMagic = "IGVC"u8.ToArray();
-    
+
     // Core Constants
 
     public static class Logging
@@ -45,7 +45,7 @@ public static class Configuration
         /// <b>NOTE:</b> This will be created if it does not exist.
         /// </summary>
         public const string PresetsDirectory = "~/.igvc/config";
-        
+
         /// <summary>
         /// The default preset name.
         /// <b>NOTE:</b> This will be created if it does not exist.
@@ -56,20 +56,20 @@ public static class Configuration
     public static class Hardware
     {
         public static bool IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-        
+
         /// <summary>
         /// The name of the interface where the Canbus is connected to.
         /// </summary>
         public const string CanbusInterface = "bigcan0";
-        
+
         /// <summary>
         /// How often to retry our connection to the Canbus.
         /// </summary>
         public static readonly TimeSpan CanbusTimeout = TimeSpan.FromMilliseconds(500);
     }
-    
+
     // Subsystem Constants
-    
+
     public static class ArcSubsystem
     {
         /// <summary>
@@ -153,7 +153,7 @@ public static class Configuration
         /// </summary>
         [Config("vision.blur_radius")]
         public const int BlurRadius = 5;
-        
+
         /// <summary>
         /// The strength of blurring we apply
         /// </summary>
@@ -182,20 +182,148 @@ public static class Configuration
         /// How long to keep the opencv calibration tool active before timing out
         /// </summary>
         public const ulong OpenCvCalibrationTimeoutMs = 60_000;
-        
+
         /// <summary>
         /// The width of the OpenCV calibration pattern (number of inner corners)
         /// </summary>
         public const int OpenCvCalibrationPatternWidth = 7;
-        
+
         /// <summary>
         /// The height of the OpenCV calibration pattern (number of inner corners)
         /// </summary>
         public const int OpenCvCalibrationPatternHeight = 7;
-        
+
         /// <summary>
         /// The size of each square in the OpenCV calibration pattern, in meters.
         /// </summary>
         public const double OpenCvCalibrationSquareSizeMeters = 0.024;
+    }
+
+    public static class FeelerSubsystem
+    {
+        // feeler-related config
+
+        /// <summary>
+        /// Default (unbiased) max length of the vision feelers, in pixels
+        /// </summary>
+        [Config("feelers.max_length")]
+        public const int MaxLength = 100;
+
+        /// <summary>
+        /// The starting angle of the 1st feeler, in degrees. 0 degrees is the positive x-axis
+        /// </summary>
+        [Config("feelers.angle_offset")]
+        public const double AngleOffset = 20;
+
+        /// <summary>
+        /// How large the arc of feelers is, in degrees (determines where the last feeler is placed)
+        /// </summary>
+        [Config("feelers.angular_width")]
+        public const double AngularWidth = 180 - 20;
+
+        /// <summary>
+        /// How many feelers to make across the specified arc.
+        /// </summary>
+        [Config("feelers.num_feelers")]
+        public const int NumFeelers = 32;
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        [Config("feelers.balace_feelers")]
+        public const bool BalanceFeelers = true;
+
+
+        // gps-related config
+        /// <summary> 
+        /// filename for the waypoints (should be CSV file with label,lat,lon,)
+        /// </summary>
+        public const string WaypointsFilename = "./data/waypoints.csv";
+
+        /// <summary>
+        /// How close we have to be for a GPS waypoint to be considered 'reached,' in meters
+        /// </summary>
+        [Config("feelers.waypoint_pop_dist")]
+        public const double WaypointPopDist = 1.5;
+
+        /// <summary>
+        /// How long we have to be within the WaypointPopDist 
+        /// </summary>
+        [Config("feelers.waypoint_pop_time")]
+        public const ulong WaypointPopTime = 500;
+
+        /// <summary>
+        /// How long to wait, after starting the run, before factoring in the GPS waypoints, in milliseconds
+        /// </summary>
+        [Config("feelers.gps_wait_time")]
+        public const ulong GpsWaitTime = 1000 * 30;
+
+        /// <summary>
+        /// The maximum amount each feeler should be biased by the GPS feeler, in pixels
+        /// </summary>
+        [Config("feelers.gps_bias_weight")]
+        public const int GpsBias = 75;
+
+        /// <summary>
+        /// The maximum amount each feeler should be biased by the forward feeler, in pixels
+        /// </summary>
+        [Config("feelers.forward_bias_weight")]
+        public const int forwardBiasWeight = 75;
+
+
+        // control-related config
+        /// <summary>
+        /// The maximum forward velocity that feelers is allowed to command, in meters per second
+        /// </summary>
+        [Config("feelers.max_drive_speed")]
+        public const double MaxDriveSpeed = 5;
+
+        /// <summary>
+        /// The maximum sideways velocity that feelers is allowed to command, in meters per second
+        /// </summary>
+        [Config("feelers.max_strafe_speed")]
+        public const double MaxStrafeSpeed = 5;
+
+        /// <summary>
+        /// The maximum rotational velocity that feelers is allowed to command, in radians per second
+        /// </summary>
+        [Config("feelers.max_turn_speed")]
+        public const double MaxTurnSpeed = 5;
+
+        /// <summary>
+        /// The proportional constant for the drive PID
+        /// </summary>
+        [Config("feelers.drive_kp")]
+        public const double DriveKp = 0.001;
+
+        /// <summary>
+        /// The integral constant for the drive PID
+        /// </summary>
+        [Config("feelers.drive_ki")]
+        public const double DriveKi = 0.0;
+
+        /// <summary>
+        /// The derivative constant for the drive PID
+        /// </summary>
+        [Config("feelers.drive_kd")]
+        public const double DriveKd = 0.0001;
+
+        /// <summary>
+        /// The proportional constant for the heading PID
+        /// </summary>
+        [Config("feelers.heading_kp")]
+        public const double HeadingKp = 0.001;
+
+        /// <summary>
+        /// The integral constant for the heading PID
+        /// </summary>
+        [Config("feelers.heading_ki")]
+        public const double HeadingKi = 0.0;
+
+        /// <summary>
+        /// The derivative constant for the heading PID
+        /// </summary>
+        [Config("feelers.heading_kd")]
+        public const double HeadingKd = 0.0001;
     }
 }
