504d9e74d7f79fad361fc34302c55dbcb010d994
Daniel Brown <is.a.squrril@gmail.com>
2026-05-07 15:49:53 -0500

    make audio subsystem play sounds on robot mode changes automatically

--- igvc_csharp/src/Subsystems/Hardware/AudioSubsystem.cs
+++ igvc_csharp/src/Subsystems/Hardware/AudioSubsystem.cs
@@ -15,10 +15,8 @@ using System.Diagnostics;
 namespace igvc_csharp.Subsystems.Hardware;
 
 
-//TODO: config
-[Subsystem("CanbusSubsystem", DependsOn = [
-    typeof(ChronosSubsystem)
-])]
+//TODO: make this subsystem like, configurable and everything
+[Subsystem("AudioSubsystem", DependsOn = [typeof(ChronosSubsystem)])]
 public class AudioSubsystem(
     SimulatorSubsystem? simulatorSubsystem,
     ChronosSubsystem chronos
@@ -38,37 +36,82 @@ public class AudioSubsystem(
             token
         );
 
+        //TODO: subscribe to Waypoint messages as well? if that code does work and gets merged...
+
         SetOperatingState(SubsystemState.Ready);
+
+        return Task.CompletedTask;
+    }
+
+    public override Task OnRobotModeChanged(RobotModeEnum old, RobotModeEnum current)
+    {
+        // check for a change in mode
+        if (old != current)
+        {
+            if (current == RobotModeEnum.Autonomous)
+            {
+                if (BaseRobot.Instance.State.Mission == MissionEnum.Autonav)
+                {
+                    PlaySound("autonav-mode.mp3");
+                }
+                else
+                {
+                    PlaySound("self-drive.mp3");
+                }
+            }
+            else if (current == RobotModeEnum.Manual)
+            {
+                PlaySound("self-drive2.mp3");
+            }
+            else if (current == RobotModeEnum.Disabled)
+            {
+                //TODO I don't think we need a sound for this (?) but it's here...
+            }
+        }
+        // no change in robot mode, so it must be something else that changed (i.e. mission or mobility)
+        else
+        {
+            if (BaseRobot.Instance.State.MotionAllowed)
+            {
+                PlaySound("mobility-enable.mp3");
+            }
+            else
+            {
+                PlaySound("mobility-stop.mp3");
+            }
+        }
 
         return Task.CompletedTask;
     }
 
     private Task OnMessageReceived(AudibleFeedback msg, CancellationToken token)
     {
-        if (msg.stop_all_sounds)
+        if (msg.StopAllSounds)
         {
             StopAllSounds(token);
         }
 
-        if (msg.filename != "")
+        if (msg.Filename != "")
         {
-            PlaySound(msg.filename, token);
+            PlaySound(msg.Filename);
         }
 
         return Task.CompletedTask;
     }
 
-    private Task PlaySound(string filename, CancellationToken token)
+    private Task PlaySound(string filename)
     {
         SetOperatingState(SubsystemState.Operating);
+
+        string relativeFilename = FileUtils.GetFileRelativeToRoot("resources/" + filename);
 
         using (Process playSound = new())
         {
             playSound.StartInfo.UseShellExecute = true;
             playSound.StartInfo.FileName = "ffplay"; //TODO FIXME
-            playSound.StartInfo.CreateNoWindow = true;
+            playSound.StartInfo.CreateNoWindow = true; // no GUI on the headless NUC
             playSound.StartInfo.ErrorDialog = false;
-            playSound.StartInfo.Arguments = " -nodisp -volume 100 -autoexit " + filename; //TODO FIXME
+            playSound.StartInfo.Arguments = " -nodisp -volume 100 -autoexit " + relativeFilename; //FIXME any other ffplay arguments we need to pass
 
             // I'm pretty sure this is non-blocking so we should keep track of it
             // there's a .WaitForExit() and a Kill() and a HasExited we can use and check
