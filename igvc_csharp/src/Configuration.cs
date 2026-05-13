3eda0b628b9028ce63a69688ace0f73325062d50
SCR <ouscr10@gmail.com>
2026-05-13 21:11:49 +0000

    don't have a UI on the NUC

--- igvc_csharp/src/Configuration.cs
+++ igvc_csharp/src/Configuration.cs
@@ -21,7 +21,7 @@ public static class Configuration
     /// Determines if the robot will use the simulator.
     /// </summary>
     [Config("simulation.enabled")]
-    public const bool UseSimulation = true;
+    public const bool UseSimulation = false;
 
     /// <summary>
     /// A magic header for all networking nonsense
