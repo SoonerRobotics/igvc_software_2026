6e4e2ea44836475ca24749ca5ddb98efb17a1f94
Dylan Zemlin <dylan.zemlin@gmail.com>
2026-05-26 16:19:12 -0500

    Add csharp service

--- setup/install_services.sh
+++ setup/install_services.sh
@@ -8,8 +8,13 @@ sudo cp misc/igvc.rules /etc/udev/rules.d/99-igvc.rules
 
 # Services
 sudo cp misc/igvc-can-slcan@.service /etc/systemd/system/igvc-can-slcan@.service
+sudo cp misc/igvc-csharp.service /etc/systemd/system/igvc-csharp.service
 
 # Reload everything
 sudo systemctl daemon-reload
 sudo systemctl restart systemd-networkd
 sudo udevadm control --reload-rules
+
+# Print how to enable igvc-csharp
+echo "To enable the igvc-csharp service, run:"
+echo "sudo systemctl enable --now igvc-csharp.service"
